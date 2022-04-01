de8b79482bbe3f2c7a7d4e934c9f47b2639845a4
dydz <dydz@dy.com>
2022-04-01 08:44:44 +0800

    [mod]BLE栈初始化

--- Src/BLE_main.c
+++ Src/BLE_main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
 #include "system_BlueNRG_LP.h"
-
+#include "ble_const.h"
+#include "bluenrg_lp_stack.h"
 
 #include "bluenrg_lp_it.h"
 #include "bsp_io.h"
+#include "BLE_config.h"
 
+NO_INIT(uint32_t dyn_alloc_a[DYNAMIC_MEMORY_SIZE>>2]);
+
+void ModulesInit(void)
+{
+	uint8_t ret;
+	BLE_STACK_InitTypeDef BLE_STACK_InitParms = BLE_STACK_INIT_PARAMETERS;
+	
+	LL_AHB_EnableClock(LL_AHB_PERIPH_PKA | LL_AHB_PERIPH_RNG);
+	
+	
+	
+}
 
 int main(void)
 {
@@ -17,8 +31,8 @@ int main(void)
 	BSP_IO_Init();
 	/* Init the UART peripheral */
 	BSP_COM_Init();
-	
-	printf("Done!\r\n");
+	/* Init BLE stack, HAL virtual timer and NVM modules */
+  ModulesInit(); 
 	
 	while(1);
 	
